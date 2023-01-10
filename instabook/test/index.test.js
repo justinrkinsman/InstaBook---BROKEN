7c133ee48bc6f06195bf35eefd4c9dd91f369040
Justin <justinrkinsman@gmail.com>
2023-01-10 13:44:20 -0400

    Remove initial test

--- instabook/test/index.test.js
+++ instabook/test/index.test.js
@@ -7,12 +7,6 @@ const express = require('express')
 app.use(express.urlencoded({ extended: false }))
 app.use('/', index)
 
-describe('Sanity test', () => {
-    test('1 should equal 1', () => {
-        expect(1).toBe(1)
-    })
-})
-
 test('Index route works', done => {
     request(app)
         .get('/test')
