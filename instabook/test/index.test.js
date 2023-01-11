88c21504ca6a7a28da42fef554a8f23a8f1037c9
Justin <justinrkinsman@gmail.com>
2023-01-11 10:53:15 -0400

    Add sign up and proceed as guest links to login page

--- instabook/test/index.test.js
+++ instabook/test/index.test.js
@@ -3,6 +3,7 @@ const index = require('../routes/index')
 const request = require('supertest')
 const app = require('../app')
 const express = require('express')
+const { get } = require('mongoose')
 
 app.use(express.urlencoded({ extended: false }))
 app.use('/', index)
@@ -10,6 +11,13 @@ app.use('/', index)
 test('Index route works', done => {
     request(app)
         .get('/')
+        .expect('Content-Type', 'text/plain; charset=utf-8')
+        .expect(302, done)
+})
+
+test('Redirect to login page if user not logged in', done => {
+    request(app)
+        .get('/login')
         .expect('Content-Type', 'text/html; charset=utf-8')
         .expect(200, done)
 })
