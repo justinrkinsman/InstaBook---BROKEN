b804d0a01e43d810ba0ea7193a96ee4e0a22ba9d
Justin <justinrkinsman@gmail.com>
2023-01-12 09:23:05 -0400

    Add passportjs support

--- instabook/app.js
+++ instabook/app.js
@@ -9,10 +9,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const User = require('./models/user')
+
 var app = express();
 
 // Set up mongoose connection
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const passport = require('passport');
 const dev_db_url = process.env.MONGO_URL
 const mongoDB = dev_db_url
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -24,6 +27,40 @@ mongoose.set('strictQuery', true) //This may cause problems
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('view engine', 'ejs')
+
+/*passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username.toLowerCase()}, (err, user) => {
+      if (err) {
+        return done(err)
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" })
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          //passwords match. log user in
+          return done(null, user)
+        } else {
+          // passwords do not match
+          return done(null, false, { message: "Incorrect password" })
+        }
+        //return done (null, user)
+      })
+    })
+  })
+)
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user)
+  })
+})
+*/
 
 app.use(logger('dev'));
 app.use(express.json());
