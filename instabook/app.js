b5cfae62393085620eb4abd3745b20764a1d8328
Justin <justinrkinsman@gmail.com>
2023-01-12 09:53:23 -0400

    Implement session parameters

--- instabook/app.js
+++ instabook/app.js
@@ -2,11 +2,16 @@ const dotenv = require('dotenv')
 dotenv.config()
 var createError = require('http-errors');
 var express = require('express');
+const session = require('cookie-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const { body, validationResult, check } = require('express-validator')
 const bcrypt = require('bcryptjs')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,7 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('view engine', 'ejs')
 
-/*passport.use(
+passport.use(
   new LocalStrategy((username, password, done) => {
     User.findOne({ username: username.toLowerCase()}, (err, user) => {
       if (err) {
@@ -62,13 +67,28 @@ passport.deserializeUser(function(id, done) {
     done(err, user)
   })
 })
-*/
 
+app.use(session({
+  secret: process.env.SESSION_PASSWORD,
+  secure: false,
+  httpOnly: true,
+  sameSite: true,
+  maxAge: 24 * 60 * 60 * 1000,
+  resave: false,
+  saveUnitialized: true, }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
