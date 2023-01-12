3a1bd2c93fed7224b4493366c155eeb4b10c0270
Justin <justinrkinsman@gmail.com>
2023-01-12 13:24:36 -0400

    Create feed.js schema for potential future use

---
+++ instabook/models/feed.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const FeedSchema = new Schema({
+    username: { type: Schema.Types.ObjectId, ref: 'User' },
+    friends_list: [{ type: Schema.Types.ObjectId, ref: 'Friends_List' }],
+    your_posts: [{ type: Schema.Types.ObjectId, ref: 'Posts' }],
+    friends_posts: [{ type: Schema.Types.ObjectId, ref: 'Posts' }]
+    //profile_pic: {type: }
+    //feed: [{type: }]
+})
+
+module.exports = mongoose.model("Feed", FeedSchema)
