0607fb65750d5168a1ecb3d20213134e10c18995
Justin <justinrkinsman@gmail.com>
2023-01-13 10:55:05 -0400

    Create route for adding new post

--- instabook/routes/api.js
+++ instabook/routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { DateTime } = require('luxon')
 
 const User = require("../models/user")
 const Post = require('../models/post')
@@ -11,6 +12,27 @@ router.get('/api/homepage', (req, res) => {
 })
 
 /// POST APIs ///
+// POST new post
+router.post('/api/posts', (req, res) => {
+    const date = new Date()
+    const newTimestamp = DateTime.fromJSDate(date).toFormat("MMMM d yyyy h:mm a")
+    postDetail = {
+        body: req.body.body,
+        author: req.body.author,
+        comments: [],
+        likes: 0,
+        timestamp: newTimestamp,
+        db_timestamp: date
+    }
+
+    let post = new Post(postDetail)
+
+    post.save(function (err) {
+        return
+    })
+    res.redirect('/')
+})
+
 // POST send friend request
 router.post('/api/user/:id', (req, res) => {
     User.updateOne( { "_id" : req.params.id }, { $push: { "friends_list.sent_requests": 'Hello'}},
--- instabook/routes/index.js
+++ instabook/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const fetch = (...args) =>
   import('node-fetch').then(({default: fetch}) => fetch(...args))
+const { DateTime } = require('luxon')
+const Post = require('../models/post')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -28,6 +30,27 @@ router.get('/homepage', function(req, res, next) {
 /* GET new post page */
 router.get('/new-post', function(req, res, next) {
   res.render('new-post.pug', {title: "New Post"})
+})
+
+/* POST new post */
+router.post('/new-post', function(req, res, next) {
+  const date = new Date()
+  const newTimestamp = DateTime.fromJSDate(date).toFormat("MMMM d yyyy h:mm a")
+  postDetail = {
+    body: req.body.body,
+    author: req.body.author,
+    comments: [],
+    likes: 0,
+    timestamp: newTimestamp,
+    db_timestamp: date
+  }
+
+  let post = new Post(postDetail)
+
+  post.save(function (err) {
+    return
+  })
+  res.redirect('/')
 })
 
 /* GET login page. */
